534fa0b6bb617f12aa06d33e64353f7f87c27539
nuri1977 <nurilacka@gmail.com>
2020-03-26 22:44:47 +0100

    Devise parameter sanitization, UI account forms styled, navbar adjusted to account loged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :url])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :url])
+  end
 end
