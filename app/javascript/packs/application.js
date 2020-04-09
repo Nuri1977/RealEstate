33919e313269f805260356efb65645ec9bb1a79c
nuri1977 <nurilacka@gmail.com>
2020-04-09 22:59:53 +0200

    Bootstrap, jquery, popper.js installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,13 @@
 
 require("@rails/ujs").start()
 
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load",() => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
