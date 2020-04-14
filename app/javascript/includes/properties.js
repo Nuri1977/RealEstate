128a6acdf6dd48044aa0dc093f9d4d2ce8103463
nuri1977 <nurilacka@gmail.com>
2020-04-14 23:55:18 +0200

    Show more button javascript toggle updated

--- app/javascript/includes/properties.js
+++ app/javascript/includes/properties.js
@@ -1,8 +1,11 @@
 $(function(){
   $("#toggleDetails").on("click", function(){
     var details = $("#prop-details");
-
     details.toggleClass("open");
-
-  })
+    if(details.hasClass("open")){
+      $(this).text("Show less");
+    } else {
+      $(this).text("Show more");
+    }
+  });
 });
