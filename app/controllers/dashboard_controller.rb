eea8d439ee06cbdd553b4f6b2802535f48fa1fb9
nuri1977 <nurilacka@gmail.com>
2020-04-14 19:24:19 +0200

    Profile view added in the Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   def index
   end
 
+  def profile
+    @account = Account.find(params[:id])
+  end
+
   def properties
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get "/dashboard" => 'dashboard#index', as: :dashboard
-  get 'dashboard/properties'
-  get 'dashboard/reports'
-  resources :properties
+
   devise_for :accounts
+  resources :properties
+
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get "/profile/:id" => 'dashboard#profile', as: :profile
+
+
   root 'public#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
