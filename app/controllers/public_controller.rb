f521e5ec05ad53a62b10b6b57a69531b2efefc56
nuri1977 <nurilacka@gmail.com>
2020-04-19 14:56:23 +0200

    Google address for property added

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,9 +1,5 @@
 class PublicController < ApplicationController
   def main
-    if account_signed_in?
-      redirect_to dashboard_path, notice: "Successfully signed in" and return
-    end
-
     @properties = Property.latest.limit(3)
   end
 
